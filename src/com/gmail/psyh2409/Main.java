bd1d4ec2ddf2bd797eadab2f797015844f8dea6e
Psyh2409 <psyh2409@gmail.com>
2020-09-27 20:34:07 +0300

    add comparators and Interface and methods

--- src/com/gmail/psyh2409/Main.java
+++ src/com/gmail/psyh2409/Main.java
@@ -2,17 +2,17 @@ package com.gmail.psyh2409;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student(Sex.male, "", "First", 0, 1);
-        Student student2 = new Student(Sex.valueOf("female"), "", "Second", 100500, 2);
-        Student student3 = new Student("Third", 3);
-        Student student4 = new Student("Fourth", 4);
-        Student student5 = new Student("Fifth", 5);
-        Student student6 = new Student("Sixth", 6);
-        Student student7 = new Student("Seventh", 7);
-        Student student8 = new Student("Eighth", 8);
-        Student student9 = new Student("Ninth", 9);
-        Student student10 = new Student("Tenth", 10);
-        Student student11 = new Student("Eleventh", 11);
+        Student student1 = new Student(Sex.male, "ewfv", "First", 100, 1);
+        Student student2 = new Student(Sex.valueOf("female"), "trfhmt", "Second", 100500, 2);
+        Student student3 = new Student(Sex.female, "sets", "Third", 16, 3);
+        Student student4 = new Student(Sex.male, "fgurmf", "Fourth", 18, 4);
+        Student student5 = new Student(Sex.male, "wsghjlo", "Fifth", 27, 5);
+        Student student6 = new Student(Sex.male, "hjg", "Sixth", 15, 6);
+        Student student7 = new Student(Sex.female, "yukht", "Seventh", 19, 7);
+        Student student8 = new Student(Sex.male, "ogfdsedtgr", "Eighth", 19, 8);
+        Student student9 = new Student(Sex.male, "edr", "Ninth", 42, 9);
+        Student student10 = new Student(Sex.female, "uoi", "Tenth", 60, 10);
+        Student student11 = new Student(Sex.male, "bmm", "Eleventh", 20, 11);
 
         Group group = new Group(new Student[]{student1, null, student3, student4, student5,
                 student6, student7, student8, student9, student10});
@@ -73,7 +73,46 @@ public class Main {
         System.out.println("group.toString();");
         System.out.println(group);
 
+        System.out.println();
+        System.out.println("group.removeStudent(3);");
+        group.removeStudent(3);
+        System.out.println("group.addStudentFromKeyboard();");
+//        try {
+//            group.addStudentFromKeyboard();
+//        } catch (RedundantStudentException e) {
+//            e.printStackTrace();
+//        }
 
+        System.out.println();
+        System.out.println("group.toString;");
+        System.out.println(group);
+
+        System.out.println();
+        group.sortByAge();
+        System.out.println("group.sortByAge();");
+        for (Student stud : group.getStudents()) {
+            System.out.println(stud);
+        }
+
+        System.out.println();
+        group.sortByName();
+        System.out.println("group.sortByName();");
+        for (Student stud : group.getStudents()) {
+            System.out.println(stud);
+        }
+
+        System.out.println();
+        group.sortBySurname();
+        System.out.println("group.sortBySurname();");
+        for (Student stud : group.getStudents()) {
+            System.out.println(stud);
+        }
+
+        System.out.println();
+        System.out.println("group.mobilisation();");
+        for (Student stud : group.mobilisation()) {
+            System.out.println(stud);
+        }
     }
 
 }
